ea364fd587d0d53252440a73c845ece67bffa219
artahseenjahan <tahseenjahan@dhcp-10-250-21-6.harvard.edu>
2026-01-14 12:33:09 -0500

    added sq and cube

--- calculator.py
+++ calculator.py
@@ -10,6 +10,11 @@ def subtract(a,b):
 def divide(a,b):
 	return a/b
 
+def square (a):
+	return (a*a)
+
+def cube (a):
+	return(a*a*a)
 
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
